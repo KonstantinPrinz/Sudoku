8d8e3871e7009a97ea405d6df3d6962bdf553fe5
Fabio Scholle <fabio.scholle@es2000.de>
2022-06-06 13:30:36 +0200

    Created struct Puzzle and FillSudoku()

--- header.h
+++ header.h
@@ -1,1 +1,8 @@
-void printsudoku(int array[9][9]);
+struct Puzzle
+{
+    int Grid[9][9];
+    // Evtl. weitere Felder: z.B. für vorgegebene Felder
+};
+
+struct Puzzle fillSudoku(struct Puzzle, int[81]);
+void printsudoku(struct Puzzle);
--- library.c
+++ library.c
@@ -3,9 +3,28 @@
 #include "header.h"
 
 /**
+Diese Funktion befüllt das Sudoku-Grid mit Werten
+**/
+struct Puzzle fillSudoku(struct Puzzle Sudoku, int Ziffern[81])
+{
+    int currentZiffer = 0;
+    for(int i=0; i<9 ; i++)
+    {
+        for(int j=0; j<9; j++)
+        {
+            Sudoku.Grid[i][j] = Ziffern[currentZiffer];
+            currentZiffer++;
+        }
+    }
+    return Sudoku;
+};
+
+
+
+/**
 Diese Funktion gibt bekommt ein 2 dimensionales Array und gibt das gesamte Sudoku aus.
 **/
-void printsudoku(int Sudoku[9][9])
+void printsudoku(struct Puzzle Sudoku)
 {
     printf("    1  2  3   4  5  6   7  8  9 \n");
     for(int col = 0; col < 9; col++)
@@ -21,13 +40,13 @@ void printsudoku(int Sudoku[9][9])
             {
                 printf("|");
             }
-            if(Sudoku[col][row] == 0)
+            if(Sudoku.Grid[col][row] == 0)
             {
                 printf(" . ");
             }
             else
             {
-                printf(" %i ", Sudoku[col][row]);
+                printf(" %i ", Sudoku.Grid[col][row]);
             }
         }
         printf("|");
